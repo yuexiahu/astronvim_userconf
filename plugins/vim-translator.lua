ab2eb76ec379a7a48e71e7ac38dc7f0494294643
huzhiheng <huzh@shuyilink.com>
2023-07-30 22:57:54 +0800

    feat: add plugin: translator

---
+++ plugins/vim-translator.lua
@@ -0,0 +1,9 @@
+return {
+  "voldikss/vim-translator",
+  keys = {
+    { "gt", "<Plug>TranslateW", desc="Translate", mode="n" },
+    { "gt", "<Plug>TranslateW", desc="Translate", mode="v" },
+    { "<F8>", "<Plug>TranslateW", desc="Translate", mode="n" },
+    { "<F8>", "<Plug>TranslateW", desc="Translate", mode="v" },
+  },
+}
